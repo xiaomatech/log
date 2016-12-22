5e1f969c265855c742c4602c581d29a62a57257f
xianhuawei <258022429@qq.com>
2016-12-22 23:57:37 +0800

    Update install_elasticsearch.sh

--- install_elasticsearch.sh
+++ install_elasticsearch.sh
@@ -4,10 +4,11 @@ version=5.1.1
 
 rpm -ivh https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$version.rpm
 /usr/share/elasticsearch/bin/elasticsearch-plugin install ingest-geoip
+/usr/share/elasticsearch/bin/elasticsearch-plugin install repository-hdfs
+/usr/share/elasticsearch/bin/elasticsearch-plugin install ingest-user-agent
 /usr/share/elasticsearch/bin/elasticsearch-plugin install lang-python
 /usr/share/elasticsearch/bin/elasticsearch-plugin install --no-verify lukas-vlcek/bigdesk
 /usr/share/elasticsearch/bin/elasticsearch-plugin install --no-verify lmenezes/elasticsearch-kopf
-/usr/share/elasticsearch/bin/elasticsearch-plugin install elasticsearch/elasticsearch-repository-hdfs
 
 
 systemctl start elasticsearch
