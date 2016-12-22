bfe5a7c4e58ad82f67f40f882db62ca1e26f5f16
xianhuawei <xianhuawei.zhcn@qq.com>
2016-12-22 21:23:02 +0800

    copy

--- install_rsyslog.sh
+++ install_rsyslog.sh
@@ -2,4 +2,9 @@
 
 yum install -y rsyslog-kafka rsyslog-mmjsonparse rsyslog-mmnormalize rsyslog-mmfields rsyslog-mmanon rsyslog-elasticsearch
 
+cp  ./rsyslog.conf /etc/
+cp -r  ./rsyslog.d /etc/
+
+rpm -ivh ./rsyslog-mmgrok-hdfs-8.23.0-1.x86_64.rpm
+
 systemctl start rsyslog
