c3b64f5835c4628aea4071f2753ee450bd6f7759
xianhuawei <258022429@qq.com>
2017-04-18 10:57:46 +0800

    Update install_elasticsearch.sh

--- install_elasticsearch.sh
+++ install_elasticsearch.sh
@@ -10,7 +10,7 @@ rpm -ivh https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ver
 /usr/share/elasticsearch/bin/elasticsearch-plugin install --no-verify lukas-vlcek/bigdesk
 /usr/share/elasticsearch/bin/elasticsearch-plugin install --no-verify lmenezes/elasticsearch-kopf
 
-/usr/share/elasticsearch/bin/elasticsearch-plugin install --no-verify l https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v1.10.3/elasticsearch-analysis-ik-1.10.3.zip
-
+/usr/share/elasticsearch/bin/elasticsearch-plugin install --no-verify https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v1.10.3/elasticsearch-analysis-ik-1.10.3.zip
+/usr/share/elasticsearch/bin/elasticsearch-plugin install --no-verify https://github.com/NLPchina/elasticsearch-sql/releases/download/5.1.2.0/elasticsearch-sql-5.1.2.0.zip
 
 systemctl start elasticsearch
