1ddca99969e636ffe47c5be3d7675e89305e8556
xianhua <xianhua@meizu.com>
2018-01-16 12:21:07 +0800

    'logstash'

--- install_logstash.sh
+++ install_logstash.sh
@@ -3,8 +3,11 @@
 version=6.1.1
 
 rpm -ivh https://artifacts.elastic.co/downloads/logstash/logstash-$version.rpm
-/usr/share/logstash/bin/logstash-plugin install --no-verify ./logstash-filter-referer-1.0.0.gem
-/usr/share/logstash/bin/logstash-plugin install --no-verify ./logstash-filter-ipip-2.0.0.gem
+/usr/share/logstash/bin/logstash-plugin install logstash-output-opentsdb
+/usr/share/logstash/bin/logstash-plugin install --no-verify https://raw.githubusercontent.com/xiaomatech/logstash-filter-ipip/master/logstash-filter-ipip-2.0.0.gem
+/usr/share/logstash/bin/logstash-plugin install --no-verify https://raw.githubusercontent.com/xiaomatech/logstash-filter-referer/master/referer-parser-0.3.0.gem
+/usr/share/logstash/bin/logstash-plugin install --no-verify https://raw.githubusercontent.com/xiaomatech/logstash-filter-referer/master/logstash-filter-referer-1.0.0.gem
+/usr/share/logstash/bin/logstash-plugin install --no-verify https://raw.githubusercontent.com/xiaomatech/logstash-filter-redis/master/logstash-filter-redis-1.0.0.gem
 sudo yum install -y GeoIP-data
 mkdir -p /data/logs/logstash
 
