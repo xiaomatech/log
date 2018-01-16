2e345a4cd7716038c8f91ddfb498a6d09d5d36e9
xianhua <xianhua@meizu.com>
2018-01-16 11:06:30 +0800

    'filebeat_metricbeat'

--- install_elasticsearch.sh
+++ install_elasticsearch.sh
@@ -4,7 +4,6 @@ version=6.1.1
 
 rpm -ivh https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$version.rpm
 /usr/share/elasticsearch/bin/elasticsearch-plugin install ingest-geoip
-/usr/share/elasticsearch/bin/elasticsearch-plugin install repository-hdfs
 /usr/share/elasticsearch/bin/elasticsearch-plugin install ingest-user-agent
 /usr/share/elasticsearch/bin/elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v$version/elasticsearch-analysis-ik-$version.zip
 /usr/share/elasticsearch/bin/elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-pinyin/releases/download/v$version/elasticsearch-analysis-pinyin-$version.zip
--- install_filebeat.sh
+++ install_filebeat.sh
@@ -5,5 +5,25 @@ version=6.1.1
 rpm -ivh https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-$version-x86_64.rpm
 rpm -ivh https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-$version-x86_64.rpm
 
+#filebeat模块的配置在/etc/filebeat/modules.d/
+cp filebeat/filebeat.yaml /etc/filebeat/filebeat.yml
+
+#metricbeat模块的配置在 /etc/metricbeat/modules.d
+cp filebeat/metricbeat.yml /etc/metricbeat/metricbeat.yml
+
+filebeat setup --dashboards
+filebeat setup --machine-learning
+filebeat setup --template
+
+metricbeat setup --dashboards
+metricbeat setup --machine-learning
+metricbeat setup --template
+
+#匹配规则在 ll /usr/share/filebeat/module/*/*/ingest/*
+#可根据特有日志格式修改对应的json
+
+systemctl enable filebeat
 systemctl start filebeat
+
+systemctl enable metricbeat
 systemctl start metricbeat
