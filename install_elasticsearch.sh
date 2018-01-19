2ed8176ac9c32c33e37bd8d953d1cac5ab5b1ec6
xianhua <xianhua@meizu.com>
2018-01-19 17:14:31 +0800

    'init'

--- install_elasticsearch.sh
+++ install_elasticsearch.sh
@@ -101,25 +101,6 @@ thread_pool.bulk.queue_size: 6000
 
 node.attr.rack: rack1
 node.attr.size: big
-
-
-index.search.slowlog.threshold.query.warn: 10s
-index.search.slowlog.threshold.query.info: 5s
-index.search.slowlog.threshold.query.debug: 2s
-index.search.slowlog.threshold.query.trace: 500ms
-
-index.search.slowlog.threshold.fetch.warn: 1s
-index.search.slowlog.threshold.fetch.info: 800ms
-index.search.slowlog.threshold.fetch.debug: 500ms
-index.search.slowlog.threshold.fetch.trace: 200ms
-
-index.indexing.slowlog.threshold.index.warn: 10s
-index.indexing.slowlog.threshold.index.info: 5s
-index.indexing.slowlog.threshold.index.debug: 2s
-index.indexing.slowlog.threshold.index.trace: 500ms
-index.indexing.slowlog.level: info
-index.indexing.slowlog.source: 1000
-
 ''' > /etc/elasticsearch/elasticsearch.yml
 
 sudo swapoff -a
